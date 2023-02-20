9e1b3278920ebbb8060a7501a348b51c1f61a56b
MizuruAndFelix <113217149+MizuruAndFelix@users.noreply.github.com>
2023-02-20 17:27:35 +0300

    1

---
+++ Out structure/Out structure.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+struct Adress 
+{
+    string Country;
+    string City;
+    string Street;
+    int HomeNum;
+    int ApartmentNum;
+    int Index;
+};
+
+void print(Adress *A)
+{
+    cout << "Адрес\n";
+    cout << "Страна: " << A->Country << endl;
+    cout << "Город: " << A->City << endl;
+    cout << "Улица: " << A->Street << endl;
+    cout << "Номер дома: " << A->HomeNum << endl;
+    cout << "Номер квартиры: " << A->ApartmentNum << endl;
+    cout << "Индекс: " << A->Index << endl;
+    cout << endl;
+}
+void fill(Adress *A)
+{
+    cout << "Введите следующие данные\n";
+    cout << "Страна: ";
+    cin >> A->Country;
+    cout << "Город: ";
+    cin >> A->City;
+    cout << "Улица: ";
+    cin >> A->Street;
+    cout << "Номер дома: ";
+    cin >> A->HomeNum;
+    cout << "Номер квартиры: ";
+    cin >> A->ApartmentNum;
+    cout << "Индекс: ";
+    cin >> A->Index;
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Adress Москва;
+    Adress Ижевск;
+    
+    fill(&Москва);
+    print(&Москва);
+    fill(&Ижевск);
+    print(&Ижевск);
+}
